c4e07485799bd57b2e89028344ed0d5d6c9bbf4a
yoav <yoavgecht@gmail.com>
2021-10-15 14:04:39 +0300

    subReddits task

--- src/App.js
+++ src/App.js
@@ -24,7 +24,24 @@ useEffect(() => {
     setSubReddits(res.data.data.children);
     console.log(res.data.data.children);
     setLoading(false);
-  })
+  }).catch(
+    function (error) {
+      if (error.response) {
+        setLoading(false);
+        // Request made and server responded
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      return <div>Error: {error.response.status}</div>
+    }
+  )
 }, []);
 
 
